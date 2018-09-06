5c84220ee5b6045e928669ffeb3999d55d1a6d98
yuan2658 <2658147108@qq.com>
2018-09-06 11:00:11 +0800

    测试套件和参数化测试

--- src/test/java/com/yuan/springbootjunit/test1/PersonControllerTest.java
+++ src/test/java/com/yuan/springbootjunit/test1/PersonControllerTest.java
@@ -9,57 +9,54 @@ import com.yuan.springbootjunit.SpringbootJunitApplication;
 import com.yuan.springbootjunit.controller.PersonController;
 import com.yuan.springbootjunit.entity.Person;
 import com.yuan.springbootjunit.service.impl.PersonServiceImpl;
+import javafx.beans.binding.When;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.*;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 
 import static org.hamcrest.Matchers.is;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringbootJunitApplication.class)
 @WebAppConfiguration
 public class PersonControllerTest {
 
-
     private MockMvc mockMvc;
 
-
-    @Spy
-    PersonServiceImpl personService;
-
-    @InjectMocks
-    PersonController personController;
+    @Autowired
+    private WebApplicationContext wac;
 
     @Before
     public void setUp() throws Exception {
-        // 初始化Mock
-        MockitoAnnotations.initMocks(this);
-        // 构造appcontext
-        this.mockMvc = MockMvcBuilders.standaloneSetup(personController).build();
+        this.mockMvc =MockMvcBuilders.webAppContextSetup(wac).build();
     }
 
     /**
      * Controller 测试
      * MockMvc可以对controller中的一次调用进行模拟，perform就是一次请求，MockMvcRequestBuilders进行url的请求，
      * andExcept方法为对Controller类、调用方法、视图和model的预期设置，andDo进行这次请求的执行，最后andReturn返回。
-     * Mockito通过方法when()、thenReturn()等方法可以对方法进行打桩，
-     * 让后续方法按照自己的数据桩来返回，达到了隔离依赖的效果。
      */
     @Test
     public void getPersonTest() throws Exception {
+        //when().thenReturn()
         int id = 1;
         mockMvc.perform(get("/person/getPerson/"+id))
                 .andDo(print())
